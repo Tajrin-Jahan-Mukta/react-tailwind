fabe0de45701d8f839bd596e9fefc2a8eb9f2d10
Tajrin-Jahan-Mukta <tajrinjahan147@gmail.com>
2023-04-20 00:33:01 +0600

    updated 20 april

--- test/state/src/App.js
+++ test/state/src/App.js
@@ -6,37 +6,52 @@ function App() {
   const[employee,setEmployee] = useState(
     [
       {
+        id: 1,
         name:'Selena',
         role:'Orthopedix',
         img:'https://images.pexels.com/photos/1382726/pexels-photo-1382726.jpeg?auto=compress&cs=tinysrgb&w=600',
       },
       {
+        id: 4,
         name:'Gigi',
         role:'Therapist',
         img:'https://images.pexels.com/photos/1382730/pexels-photo-1382730.jpeg?auto=compress&cs=tinysrgb&w=600',
       },
       {
+        id: 3,
         name:'Bella',
         role:'Heart Specialist',
         img:'https://images.pexels.com/photos/1462636/pexels-photo-1462636.jpeg?auto=compress&cs=tinysrgb&w=600',
       },
       {
+        id: 2,
         name:'Mac',
         role:'Orthopedix',
         img:'https://images.pexels.com/photos/5327656/pexels-photo-5327656.jpeg?auto=compress&cs=tinysrgb&w=600',
       },
       {
+        id: 7,
         name:'Jack',
         role:'Therapist',
         img:'https://images.pexels.com/photos/5452268/pexels-photo-5452268.jpeg?auto=compress&cs=tinysrgb&w=600',
       },
       {
+        id: 8,
         name:'Harry',
         role:'Heart Specialist',
         img:'https://images.pexels.com/photos/4173239/pexels-photo-4173239.jpeg?auto=compress&cs=tinysrgb&w=600',
       },
     ]
   );
+  function updateEmployee(id,newName,newRole){
+    const updatedEmployees= employee.map((employee) => {
+      if(id==employee.id){
+        return {...employee, name: newName, role: newRole}
+      }
+      return employee;
+
+    })
+  }
   return (
     <div>
       <>
@@ -50,9 +65,13 @@ function App() {
         {employee.map((employee) => {
           console.log(employee);
           return(
-            <Doctor name={employee.name}
+            <Doctor
+            key={employee.id} 
+            id={employee.id}
+            name={employee.name}
             role={employee.role}
             img={employee.img}
+            updateEmployee={updateEmployee}
             />
           );
         })}
--- test/state/src/Component/EditEmployee.js
+++ test/state/src/Component/EditEmployee.js
@@ -2,7 +2,10 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
+function EditEmployee(props) {
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
+  
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -10,10 +13,10 @@ function EditEmployee() {
 
   return (
     <>
-    <button onClick={handleShow} class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+      <button onClick={handleShow} class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
         Appointment
-        </button>
-      
+      </button>
+
 
       <Modal
         show={show}
@@ -22,17 +25,53 @@ function EditEmployee() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don not even try to press
-          escape key.
+          <div className="w-full max-w-xs">
+            <form 
+            onSubmit={(e)=> {
+              e.preventDefault();
+              console.log('hello from edit Employee');
+              console.log(props.id,name,role);
+              props.updateEmployee(props.id,name,role);
+            }}
+            id="editModal" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
+                  name
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id="name" 
+                type="text" 
+                value={name}
+                onChange={(e) => {setName(e.target.value);
+                }}
+                 />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="role">
+                  role
+                </label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                id="role" 
+                type="text" 
+                value={role} 
+                onChange={(e) => {setRole(e.target.value);
+                }}/>
+              </div>
+
+            </form>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            form="editModal">
+            Update
+          </button>
         </Modal.Footer>
       </Modal>
     </>
